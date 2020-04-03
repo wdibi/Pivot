506937e51cc048bd1a311e68df162385bf756f5b
Nicolas Raymundo <nicoraymundo2@gmail.com>
2020-04-02 23:16:23 -0700

    Update logo

--- website/pivot-website/src/Home.js
+++ website/pivot-website/src/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 import Fade from "react-reveal/Fade";
 
 import "./Home.css";
+import pivot from "./Images/Pivot/pivot.png";
 
 const Home = () => {
   return (
     <Fade clear duration={4000}>
       <div className="main-paragraph">
+        <img src={pivot} alt="logo top" className="home-logo" />
         <p className="intro-content">A new spin on programming</p>
       </div>
     </Fade>
