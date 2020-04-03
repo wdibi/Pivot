36417c9c76fd50bd24ddd97281843b0cdac0e029
Nicolas Raymundo <nicoraymundo2@gmail.com>
2020-04-02 23:15:29 -0700

    Update about content

--- website/pivot-website/src/About.js
+++ website/pivot-website/src/About.js
@@ -2,7 +2,10 @@ import React from "react";
 import Fade from "react-reveal/Fade";
 
 import "./About.css";
-import jonah from "./Images/Filler/jonah.jpg";
+import nico from "./Images/Team/nico.jpg";
+import will from "./Images/Team/will.png";
+import jigar from "./Images/Team/jigar.png";
+import manny from "./Images/Team/manny.png";
 
 const About = () => {
   return (
@@ -16,26 +19,30 @@ const About = () => {
           Awk, F#, Erlang, and JS, Pivot takes an impure approach with the
           mindset of removing the limitations of similar languages.
         </p>
+        <div className="about-buttons">
+          <button className="repo-button">Visit our GitHub!</button>
+          <button className="repo-button">See Our Grammar!</button>
+        </div>
       </div>
       <div className="skills-container">
         <h1 className="about-title">Meet The Team</h1>
         <hr className="horizontal-line" />
         <div className="skills-content">
           <div className="about-profile">
-            <img src={jonah} alt="jonah" className="playroll-img" />
+            <img src={manny} alt="manny" className="playroll-img" />
             <h2 className="list-title">Manny Barreto</h2>
           </div>
           <div className="about-profile">
+            <img src={will} alt="will" className="playroll-img" />
             <h2 className="list-title">Will DiBiagio</h2>
-            <img src={jonah} alt="jonah" className="playroll-img" />
           </div>
           <div className="about-profile">
+            <img src={nico} alt="nico" className="playroll-img" />
             <h2 className="list-title">Nicolas Raymundo</h2>
-            <img src={jonah} alt="jonah" className="playroll-img" />
           </div>
           <div className="about-profile">
+            <img src={jigar} alt="jigar" className="playroll-img" />
             <h2 className="list-title">Jigar Swaminarayan</h2>
-            <img src={jonah} alt="jonah" className="playroll-img" />
           </div>
         </div>
       </div>
