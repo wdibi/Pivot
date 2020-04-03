a35f523f953b5ffb9f8dfdbacd12388dd3494631
Nicolas Raymundo <nicoraymundo2@gmail.com>
2020-04-02 23:50:45 -0700

    Update spacing

--- website/pivot-website/src/AppHeader.js
+++ website/pivot-website/src/AppHeader.js
@@ -3,12 +3,14 @@ import Navbar from "react-bootstrap/NavBar";
 import Nav from "react-bootstrap/Nav";
 
 import "./AppHeader.css";
+import pivot from "./Images/Pivot/pivot.png";
 
 const AppHeader = ({ setCurrDisplay }) => {
   return (
     <Navbar>
-      <Nav.Link onClick={() => setCurrDisplay("intro")} className="logo">
+      <Nav.Link onClick={() => setCurrDisplay("home")} className="logo">
         Pivot
+        {/* <img src={pivot} alt="logo top" className="header-logo" /> */}
       </Nav.Link>
       <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
         <Nav className="navcontent">
