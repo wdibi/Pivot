abfcb2006755d7cd3fe9a8028d217429d642b21e
Will DiBiagio <wdibiagio@gmail.com>
2020-04-05 01:47:33 -0500

    Add variabledec

--- semantics/__test__/semantic_errors.test.js
+++ semantics/__test__/semantic_errors.test.js
@@ -1,21 +1,14 @@
-/*
- * Semantic Error Tests
- *
- * These tests check that the analyzer will reject programs with various
- * static semantic errors.
- */
-
 const parse = require('../../ast/parser');
 const Context = require('../context');
 
 const errors = [
-  ['use of undeclared variable', 'x <- 1'],
-  ['type mismatch in declaration', 'bool x <- 5'],
+  ['type mismatch in declaration', 'num x <- a;'],
+  ['variable already declared', 'bool x <- true; bool x <- false;'],
 ];
 
 describe('The semantic analyzer', () => {
   errors.forEach(([scenario, program]) => {
-    test(`detects the error ${scenario}`, done => {
+    test(`detects the error ${scenario}`, (done) => {
       const astRoot = parse(program);
       expect(astRoot).toBeTruthy();
       expect(() => astRoot.analyze(Context.INITIAL)).toThrow();
---
+++ semantics/context.js
@@ -0,0 +1,84 @@
+const {
+  standardFunctions,
+  BoolType,
+  CharType,
+  StringType,
+  NumType,
+  AutoType,
+  ListType,
+  DictType,
+} = require('./builtins');
+
+require('./analyzer');
+
+class Context {
+  constructor({ parent = null, currentFunction = null, inLoop = false } = {}) {
+    Object.assign(this, {
+      parent,
+      currentFunction,
+      inLoop,
+      locals: new Map(),
+    });
+  }
+
+  createChildContextForFunctionBody(currentFunction) {
+    // When entering a new function, we're not in a loop anymore
+    return new Context({ parent: this, currentFunction, inLoop: false });
+  }
+
+  createChildContextForLoop() {
+    // When entering a loop body, just set the inLoop field, retain others
+    return new Context({
+      parent: this,
+      currentFunction: this.currentFunction,
+      inLoop: true,
+    });
+  }
+
+  createChildContextForBlock() {
+    // For a block, we have to retain both the function and loop settings.
+    return new Context({
+      parent: this,
+      currentFunction: this.currentFunction,
+      inLoop: this.inLoop,
+    });
+  }
+
+  // Adds a declaration to this context.
+  add(declaration) {
+    declaration.id = declaration.id.id || declaration.id; // I know, this needs to be fixed...
+    if (this.locals.has(declaration.id)) {
+      throw new Error(`${declaration.id} already declared in this scope`);
+    }
+    const entity = declaration;
+    this.locals.set(declaration.id, entity);
+  }
+
+  // Returns the entity bound to the given identifier, starting from this
+  // context and searching "outward" through enclosing contexts if necessary.
+  lookup(id) {
+    for (let context = this; context !== null; context = context.parent) {
+      if (context.locals.has(id)) {
+        return context.locals.get(id);
+      }
+    }
+    throw new Error(`Identifier ${id} has not been declared`);
+  }
+}
+
+Context.INITIAL = new Context();
+
+[
+  BoolType,
+  CharType,
+  StringType,
+  NumType,
+  AutoType,
+  // ListType,
+  // DictType,
+  ...standardFunctions,
+].forEach((entity) => {
+  Context.INITIAL.add(entity);
+});
+
+module.exports = Context;
