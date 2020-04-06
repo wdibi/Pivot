155f4989b7b4defb965f1c0f31c9f1b70461073b
mannybarreto <mbarre13@lion.lmu.edu>
2020-04-05 20:51:56 -0400

    Node text fixes by rtoal

--- semantics/viewer.js
+++ semantics/viewer.js
@@ -3,28 +3,36 @@ const d3n = require('d3-node');
 const d3 = d3n.d3;
 
 module.exports = root => {
-  const dataset = { entities: [], edges: [] }
+  const dataset = { entities: [], edges: [] };
   addReachableEntities(root, dataset, -1);
-  return dataset; 
+  dataset.entities = dataset.entities.map(e => nodeText(e));
+  return dataset;
 };
 
+function nodeText(node) {
+  let text = node.constructor.name;
+  Object.entries(node).forEach(([prop, value]) => {
+    if (['boolean', 'number', 'string'].includes(typeof value)) {
+      text += ` ${prop}=${value}`;
+    }
+  });
+  return text;
+}
 
 function addReachableEntities(node, dataset, parentIndex, label) {
-  if (node === null || typeof node !== 'object' || dataset.entities.includes(node)) {
+  if (dataset.entities.includes(node)) {
+    dataset.edges.push({ source: parentIndex, target: dataset.entities.indexOf(node), label });
     return;
   }
-
   dataset.entities.push(node);
   const nodeIndex = dataset.entities.length - 1;
-
   if (parentIndex >= 0) {
-    dataset.edges.push({ source: parentIndex, target: nodeIndex, label: label });
+    dataset.edges.push({ source: parentIndex, target: nodeIndex, label });
   }
-
   Object.entries(node).forEach(([prop, value]) => {
     if (Array.isArray(value)) {
       value.forEach((n, i) => addReachableEntities(n, dataset, nodeIndex, `${prop}[${i}]`));
-    } else {
+    } else if (typeof value === 'object' && value !== null) {
       addReachableEntities(value, dataset, nodeIndex, prop);
     }
   });
