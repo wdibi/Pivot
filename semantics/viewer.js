f932869d07c99133b2ea507e61d192f628b65f5e
mannybarreto <mbarre13@lion.lmu.edu>
2020-04-06 16:21:00 -0400

    Getting close, not properly rendering svg however

--- semantics/viewer.js
+++ semantics/viewer.js
@@ -1,22 +1,24 @@
-const D3Node = require('d3-node')
+const d3 = require('d3');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 const fs = require('fs');
 
 module.exports = root => {
   const dataset = { entities: [], edges: [] };
   addReachableEntities(root, dataset, -1);
-  dataset.entities = dataset.entities.map(e => nodeText(e));
+  dataset.entities = dataset.entities.map((e, i) => nodeText(e, i));
   visualizeAST(dataset);
   return 'success'
 };
 
-function nodeText(node) {
+function nodeText(node, index) {
   let text = node.constructor.name;
   Object.entries(node).forEach(([prop, value]) => {
     if (['boolean', 'number', 'string'].includes(typeof value)) {
       text += ` ${prop}=${value}`;
     }
   });
-  return text;
+  return {name: text, id: index};
 }
 
 function addReachableEntities(node, dataset, parentIndex, label) {
@@ -39,85 +41,74 @@ function addReachableEntities(node, dataset, parentIndex, label) {
 }
 
 function visualizeAST(dataset) {
-  const d3n = new D3Node()
-  const height = 1080, width = 1920;
-  const svg = d3n.createSVG(width, height)
+  const width = 1920, height = 1080;
+  const document = new JSDOM(`<!DOCTYPE html><html><body></body></html>`).window.document;
 
-  const edges = svg.selectAll('line')
+  const simulation = d3.forceSimulation(dataset.entities)
+    .force("link", d3.forceLink(dataset.edges).id((d, i) => i))
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width / 2, height / 2));
+
+  const svg = d3.select(document).select('body')
+    .append('div')
+    .attr('class', 'container')
+      .append("svg")
+      .attr("viewBox", [0, 0, width, height]);
+
+  const link = svg.append("g")
+    .attr("stroke", "#999")
+    .attr("stroke-opacity", 0.6)
+    .selectAll("line")
     .data(dataset.edges)
-    .enter()
-    .append('line')
-    .attr('id',function(d,i) {return 'edge'+i})
-    .attr('marker-end','url(#arrowhead)')
-    .style('stroke','#ccc')
-    .style('pointer-events', 'none');
-    
-  const entities = svg.selectAll('circle')
+    .join("line")
+
+  const node = svg.append("g")
+    .attr("stroke", "#fff")
+    .attr("stroke-width", 1.5)
+    .selectAll("circle")
     .data(dataset.entities)
-    .enter()
-    .append('circle')
-    .attr({'r':15})
-    // .style('fill',function(d,i){return colors(i);})
-    // .call(force.drag)
+    .join("circle")
+    .attr("r", 5)
+    .call((simulation) => {
+      function dragstarted(d) {
+        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+      }
+      
+      function dragged(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+      }
+      
+      function dragended(d) {
+        if (!d3.event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+      }
+      
+      return d3.drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended);
+    });
 
-  const entitylabels = svg.selectAll('.entitylabel') 
-    .data(dataset.entities)
-    .enter()
-    .append('text')
-    .text( (d) => { return d.name; } )
-    .attr({'x': (d) => { return d.x; },
-          'y': (d) => { return d.y; },
-          'class':'entitylabel',
-          'stroke':'black'});
+  node.append("title")
+    .text(d => d.name);
 
-  const edgepaths = svg.selectAll('.edgepath')
-    .data(dataset.edges)
-    .enter()
-    .append('path')
-    .style('pointer-events', 'none')
-    .attr({'d': (d) => { return 'M '+d.source.x + ' '+ d.source.y+' L '+ d.target.x +' '+ d.target.y },
-          'class':'edgepath',
-          'fill-opacity':0,
-          'stroke-opacity':0,
-          'fill':'blue',
-          'stroke':'red',
-          'id': (d,i) => {return 'edgepath'+i}});
+  simulation.on("tick", () => {
+    link
+      .attr("x1", d => d.source.x)
+      .attr("y1", d => d.source.y)
+      .attr("x2", d => d.target.x)
+      .attr("y2", d => d.target.y);
 
-  const edgelabels = svg.selectAll('.edgelabel')
-    .data(dataset.edges)
-    .enter()
-    .append('text')
-    .style('pointer-events', 'none')
-    .attr({'class':'edgelabel',
-          'id':(d,i) => { return 'edgelabel' + i },
-          'dx':80,
-          'dy':0,
-          'font-size':10,
-          'fill':'#aaa'});
+    node
+      .attr("cx", d => d.x)
+      .attr("cy", d => d.y);
+  });
 
-  // edgelabels.append('textPath')
-  //   .attr('xlink:href',(d,i) => { return '#edgepath' + i })
-  //   .style('pointer-events', 'none')
-  //   .text((d,i) => { return 'label ' + i });
+  fs.writeFileSync('test.svg', d3.select(document).select('body').select('.container').html()) //using sync to keep the code simple
 
-  // svg.append('defs').append('marker')
-  //   .attr({'id':'arrowhead',
-  //              'viewBox':'-0 -5 10 10',
-  //              'refX':25,
-  //              'refY':0,
-  //              'orient':'auto',
-  //              'markerWidth':10,
-  //              'markerHeight':10,
-  //              'xoverflow':'visible'})
-  //   .append('svg:path')
-  //   .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
-  //   .attr('fill', '#ccc')
-  //   .attr('stroke','#ccc');
-
-  fs.writeFile('./test.html', d3n.html(), function(err) {
-    if(err) {
-        return console.log(err);
-    }
-    console.log("The file was saved!");
-  }); 
+  return;
 }
