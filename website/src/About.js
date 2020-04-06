598dd74cc7f66593da528faeba98e45bc57bebeb
Nicolas Raymundo <nicoraymundo2@gmail.com>
2020-04-05 18:09:38 -0700

    Added cards for team/code snippets

--- website/src/About.js
+++ website/src/About.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Card, Image } from "semantic-ui-react";
 
 import "./About.css";
 import nico from "./Images/Team/nico.jpg";
-import will from "./Images/Team/will.png";
-import jigar from "./Images/Team/jigar.png";
-import manny from "./Images/Team/manny.png";
+import will from "./Images/Team/will.jpg";
+import jigar from "./Images/Team/jigar.jpg";
+import manny from "./Images/Team/manny.jpg";
 
 const About = () => {
   return (
@@ -41,22 +42,94 @@ const About = () => {
         <h1 className="about-title">Meet The Team</h1>
         <hr className="horizontal-line" />
         <div className="team-content">
-          <div className="member-profile">
-            <img src={manny} alt="manny" className="member-img" />
-            <h2 className="member-name">Manny Barreto</h2>
-          </div>
-          <div className="member-profile">
-            <img src={will} alt="will" className="member-img" />
-            <h2 className="member-name">Will DiBiagio</h2>
-          </div>
-          <div className="member-profile">
-            <img src={nico} alt="nico" className="member-img" />
-            <h2 className="member-name">Nicolas Raymundo</h2>
-          </div>
-          <div className="member-profile">
-            <img src={jigar} alt="jigar" className="member-img" />
-            <h2 className="member-name">Jigar Swaminarayan</h2>
-          </div>
+          <Card className="member-card">
+            <Image src={manny} wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>Manny Barreto</Card.Header>
+            </Card.Content>
+            <Card.Content extra className="about-card-content">
+              <a
+                href="https://github.com/mannybarreto"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+              <a
+                href="https://www.linkedin.com/in/manny-barreto/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+            </Card.Content>
+          </Card>
+          <Card className="member-card">
+            <Image src={will} wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>Will DiBiagio</Card.Header>
+            </Card.Content>
+            <Card.Content extra className="about-card-content">
+              <a
+                href="https://github.com/wdibi"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+              <a
+                href="https://www.linkedin.com/in/wdibi/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+            </Card.Content>
+          </Card>
+          <Card className="member-card">
+            <Image src={nico} wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>Nicolas Raymundo</Card.Header>
+            </Card.Content>
+            <Card.Content extra className="about-card-content">
+              <a
+                href="https://github.com/nraymundo"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+              <a
+                href="https://www.linkedin.com/in/nicolasraymundo/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+            </Card.Content>
+          </Card>
+          <Card className="member-card">
+            <Image src={jigar} wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>Jigar Swaminarayan</Card.Header>
+            </Card.Content>
+            <Card.Content extra className="about-card-content">
+              <a
+                href="https://github.com/JigarSwam"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+              <a
+                href="https://www.linkedin.com/in/jigarswaminarayan/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+            </Card.Content>
+          </Card>
         </div>
       </div>
     </div>
--- website/src/Code.js
+++ website/src/Code.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Card } from "semantic-ui-react";
 
 import "./Code.css";
 
@@ -11,56 +12,92 @@ const Code = () => {
       </div>
       <div className="code-list">
         <div className="snippet-container">
-          <h1 className="snippet-title">Even or Odd</h1>
-          {/* <hr className="horizontal-line-projects" /> */}
-          <pre>{`
-            evenOdd(num x) -> bool
-                return x % 2 == 0;
-            end
-          `}</pre>
+          <Card className="code-box">
+            <Card.Content className="code-content">
+              <Card.Header className="snippet-title">Even or Odd</Card.Header>
+              <Card.Description className="snippet-code">
+                <pre>
+                  {`evenOdd(num x) -> bool`}
+                  <br />
+                  {`    return x % 2 == 0;`}
+                  <br />
+                  {`end`}
+                </pre>
+              </Card.Description>
+            </Card.Content>
+          </Card>
         </div>
         <div className="snippet-container">
-          <h1 className="snippet-title">Greatest Common Divisor</h1>
-          {/* <hr className="horizontal-line-projects" /> */}
-          <pre>
-            {`
-            gcd(num a, num b) -> num
-                return a when !b otherwise gcd(b, a % b);
-            end
-            `}
-          </pre>
+          <Card className="code-box">
+            <Card.Content className="code-content">
+              <Card.Header className="snippet-title">
+                Greatest Common Denominator
+              </Card.Header>
+              <Card.Description className="snippet-code">
+                <pre>
+                  {`gcd(num a, num b) -> num`}
+                  <br />
+                  {`    return a when !b otherwise gcd(b, a % b);`}
+                  <br />
+                  {`end`}
+                </pre>
+              </Card.Description>
+            </Card.Content>
+          </Card>
         </div>
         <div className="snippet-container">
-          <h1 className="snippet-title">Fibonacci</h1>
-          {/* <hr className="horizontal-line-projects" /> */}
-          <pre>
-            {`
-            fibonacci(num x) -> num
-                all num a, b, temp <- 1, 0, 0;
-                repeat
-                    temp <- a;
-                    a <- a + b;
-                    b <- temp;
-                    x <- x - 1;
-                until num < 0;
-                return b;
-            end
-            `}
-          </pre>
+          <Card className="code-box">
+            <Card.Content className="code-content">
+              <Card.Header className="snippet-title">Fibonacci</Card.Header>
+              <Card.Description className="snippet-code">
+                <pre>
+                  {`fibonacci(num x) -> num`}
+                  <br />
+                  {`    all num a, b, temp <- 1, 0, 0;`}
+                  <br />
+                  {`    repeat`}
+                  <br />
+                  {`        temp <- a;`}
+                  <br />
+                  {`        a <- a + b;`}
+                  <br />
+                  {`        b <- temp;`}
+                  <br />
+                  {`        x <- x - 1;`}
+                  <br />
+                  {`    until num < 0;`}
+                  <br />
+                  {`    return b;`}
+                  <br />
+                  {`end`}
+                </pre>
+              </Card.Description>
+            </Card.Content>
+          </Card>
         </div>
         <div className="snippet-container">
-          <h1 className="snippet-title">First Factorial</h1>
-          {/* <hr className="horizontal-line-projects" /> */}
-          <pre>
-            {`
-            firstFactorial(num x) -> num
-                if x == 0 or x == 1
-                    then return 1;
-                else
-                    return x * firstFactorial(x - 1);
-            end
-            `}
-          </pre>
+          <Card className="code-box">
+            <Card.Content className="code-content">
+              <Card.Header className="snippet-title">
+                First Factorial
+              </Card.Header>
+              <Card.Description className="snippet-code">
+                <pre>
+                  {`firstFactorial(num x) -> num`}
+                  <br />
+                  {`    if x == 0 or x == 1`}
+                  <br />
+                  {`        then return 1;`}
+                  <br />
+                  {`    else`}
+                  <br />
+                  {`        return x * firstFactorial(x - 1);`}
+                  <br />
+                  {`end`}
+                </pre>
+              </Card.Description>
+            </Card.Content>
+          </Card>
         </div>
       </div>
     </div>
