61f46f0fd92fb4f27a700246c13b16e46b942557
Will DiBiagio <wdibiagio@gmail.com>
2020-04-07 17:15:23 -0500

    Add a lit to type matcher

--- semantics/check.js
+++ semantics/check.js
@@ -11,10 +11,19 @@ function doCheck(condition, message) {
 module.exports = {
   isNotVariableTypeMismatch(type, expression) {
     doCheck(
-      expression instanceof NumericLiteral && type.id === NumType.id,
+      type.id === LiteralTypeMatch(expression).id,
       `Expression of type ${util.format(
         expression.type
       )} not compatible with type ${util.format(type)}`
     );
   },
+};
+
+const LiteralTypeMatch = (literal) => {
+  switch (literal.constructor) {
+    case NumericLiteral:
+      return NumType;
+    default:
+      throw new Error('ugh');
+  }
 };
