06006c6301c30ab5ecff7732f1d9298eda0e0963
mannybarreto <mbarre13@lion.lmu.edu>
2020-04-07 19:23:52 -0400

    Now rendering basic static graph

--- semantics/viewer.js
+++ semantics/viewer.js
@@ -3,6 +3,8 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const fs = require('fs');
 
+const width = 1920, height = 1080;
+
 module.exports = root => {
   const dataset = { entities: [], edges: [] };
   addReachableEntities(root, dataset, -1);
@@ -18,7 +20,7 @@ function nodeText(node, index) {
       text += ` ${prop}=${value}`;
     }
   });
-  return {name: text, id: index};
+  return {name: text, id: index, x: Math.floor(Math.random() * width), y: Math.floor(Math.random() * height)};
 }
 
 function addReachableEntities(node, dataset, parentIndex, label) {
@@ -29,7 +31,7 @@ function addReachableEntities(node, dataset, parentIndex, label) {
   dataset.entities.push(node);
   const nodeIndex = dataset.entities.length - 1;
   if (parentIndex >= 0) {
-    dataset.edges.push({ source: parentIndex, target: nodeIndex, label });
+    dataset.edges.push({ source: parentIndex, target: nodeIndex, label: label });
   }
   Object.entries(node).forEach(([prop, value]) => {
     if (Array.isArray(value)) {
@@ -41,72 +43,45 @@ function addReachableEntities(node, dataset, parentIndex, label) {
 }
 
 function visualizeAST(dataset) {
-  const width = 1920, height = 1080;
   const document = new JSDOM(`<!DOCTYPE html><html><body></body></html>`);
-
-  const simulation = d3.forceSimulation(dataset.entities)
-    .force("link", d3.forceLink(dataset.edges).id((d, i) => i))
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2));
 
   const svg = d3.select(document.window.document).select('body')
     .append('div')
     .attr('class', 'container')
-      .append("svg")
-      .attr("viewBox", [0, 0, width, height]);
+    .append('svg')
+    .attr('viewBox', [0, 0, width, height]);
 
-  const link = svg.append("g")
-    .attr("stroke", "#999")
-    .attr("stroke-opacity", 0.6)
-    .selectAll("line")
+  const edge = svg.append('g')
+    .attr('stroke', '#999')
+    .attr('stroke-opacity', 0.6)
+    .selectAll('line')
     .data(dataset.edges)
-    .join("line")
+    .enter().append("line")
+    .attr('x1', d => dataset.entities[d.source].x)
+    .attr('y1', d => dataset.entities[d.source].y)
+    .attr('x2', d => dataset.entities[d.target].x)
+    .attr('y2', d => dataset.entities[d.target].y);
 
-  const node = svg.append("g")
-    .attr("stroke", "#fff")
-    .attr("stroke-width", 1.5)
-    .selectAll("circle")
+  const entities = svg.append("g")
+    .attr('stroke', '#fff')
+    .attr('stroke-width', 1.5)
+    .selectAll('circle')
     .data(dataset.entities)
-    .join("circle")
-    .attr("r", 5)
-    .call((simulation) => {
-      function dragstarted(d) {
-        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-        d.fx = d.x;
-        d.fy = d.y;
-      }
-      
-      function dragged(d) {
-        d.fx = d3.event.x;
-        d.fy = d3.event.y;
-      }
-      
-      function dragended(d) {
-        if (!d3.event.active) simulation.alphaTarget(0);
-        d.fx = null;
-        d.fy = null;
-      }
-      
-      return d3.drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended);
-    });
+    .join('circle')
+    .attr('r', 10)
+    .attr('cx', d => d.x)
+    .attr('cy', d => d.y);
 
-  node.append("title")
-    .text(d => d.name);
-
-  simulation.on("tick", () => {
-    link
-      .attr("x1", d => d.source.x)
-      .attr("y1", d => d.source.y)
-      .attr("x2", d => d.target.x)
-      .attr("y2", d => d.target.y);
-
-    node
-      .attr("cx", d => d.x)
-      .attr("cy", d => d.y);
-  });
+  const entityLabels = svg.append('g')
+    .selectAll('.entityLabel')
+    .data(dataset.entities)
+    .enter()
+    .append('text')
+    .attr('x', d => d.x + 10)
+    .attr('y', d => d.y)
+    .attr('class', 'entityLabel')
+    .text(d => d.name)
+    .attr("font-family", "monospace").attr("font-size", "14px").attr("fill", "black");
 
   fs.writeFileSync('test.html', document.serialize()) //using sync to keep the code simple
   
