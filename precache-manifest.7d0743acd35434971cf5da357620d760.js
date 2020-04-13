4b62f4738e1d9ee0ef0e7d22841b59bfecccc4bc
Will DiBiagio <wdibiagio@gmail.com>
2020-04-13 17:24:41 -0500

    Updates

---
+++ precache-manifest.7d0743acd35434971cf5da357620d760.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1326edc4abba2bbb1128f189fd60ac58",
+    "url": "/%7Bpivot%7D/index.html"
+  },
+  {
+    "revision": "0b292baa577bba6d3b91",
+    "url": "/%7Bpivot%7D/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "830cd5818af7662b5347",
+    "url": "/%7Bpivot%7D/static/css/main.a0aebacd.chunk.css"
+  },
+  {
+    "revision": "0b292baa577bba6d3b91",
+    "url": "/%7Bpivot%7D/static/js/2.24975335.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/%7Bpivot%7D/static/js/2.24975335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "830cd5818af7662b5347",
+    "url": "/%7Bpivot%7D/static/js/main.5a97ca33.chunk.js"
+  },
+  {
+    "revision": "102d31bad36e3f22b599",
+    "url": "/%7Bpivot%7D/static/js/runtime-main.c912ab5d.js"
+  },
+  {
+    "revision": "31d986118ee86d0bf9885ab43b61cab1",
+    "url": "/%7Bpivot%7D/static/media/jigar.31d98611.jpg"
+  },
+  {
+    "revision": "f40355bbb79c6e9f43b7f4308301934e",
+    "url": "/%7Bpivot%7D/static/media/manny.f40355bb.jpg"
+  },
+  {
+    "revision": "8b382cf3d3c34cf3a6a45085a5cc5773",
+    "url": "/%7Bpivot%7D/static/media/nico.8b382cf3.jpg"
+  },
+  {
+    "revision": "b9c79696eab8103059b7b85efb666640",
+    "url": "/%7Bpivot%7D/static/media/pivot.b9c79696.png"
+  },
+  {
+    "revision": "9aca70c0397aec400f9762bd7bcfa27b",
+    "url": "/%7Bpivot%7D/static/media/will.9aca70c0.jpg"
+  }
+]);
