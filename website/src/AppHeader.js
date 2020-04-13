a8ad65f3da44fa70be44ea0a2620f6148ad23fc2
Will DiBiagio <wdibiagio@gmail.com>
2020-04-13 18:12:40 -0500

    remove comments

--- website/src/AppHeader.js
+++ website/src/AppHeader.js
@@ -1,26 +1,24 @@
-import React from "react";
-import Navbar from "react-bootstrap/NavBar";
-import Nav from "react-bootstrap/Nav";
+import React from 'react';
+import Navbar from 'react-bootstrap/NavBar';
+import Nav from 'react-bootstrap/Nav';
 
-import "./AppHeader.css";
-// import pivot from "./Images/Pivot/pivot.png";
+import './AppHeader.css';
 
 const AppHeader = ({ setCurrDisplay }) => {
   return (
     <Navbar>
-      <Nav.Link onClick={() => setCurrDisplay("home")} className="logo">
+      <Nav.Link onClick={() => setCurrDisplay('home')} className="logo">
         Pivot
-        {/* <img src={pivot} alt="logo top" className="header-logo" /> */}
       </Nav.Link>
       <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
         <Nav className="navcontent">
-          <Nav.Link onClick={() => setCurrDisplay("home")} className="NavLink">
+          <Nav.Link onClick={() => setCurrDisplay('home')} className="NavLink">
             Home
           </Nav.Link>
-          <Nav.Link onClick={() => setCurrDisplay("about")} className="NavLink">
+          <Nav.Link onClick={() => setCurrDisplay('about')} className="NavLink">
             About
           </Nav.Link>
-          <Nav.Link onClick={() => setCurrDisplay("code")} className="NavLink">
+          <Nav.Link onClick={() => setCurrDisplay('code')} className="NavLink">
             Code Examples
           </Nav.Link>
         </Nav>
