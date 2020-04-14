fb369f4c067b2c3747dc8ff2d0f2e77e2044307c
Will DiBiagio <wdibiagio@gmail.com>
2020-04-14 01:46:39 -0500

    update!

--- website/src/App.js
+++ website/src/App.js
@@ -7,7 +7,6 @@ import Home from "./Home";
 import About from "./About";
 import Code from "./Code";
 
-// TEST Deply Script
 const App = () => {
   let [currDisplay, setCurrDisplay] = useState("home");
 
