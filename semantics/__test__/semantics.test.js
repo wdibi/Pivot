2a59ebf6f73b7ef3804fda7c94f3b8c3ea422ff3
mannybarreto <mbarre13@lion.lmu.edu>
2020-04-14 21:01:33 -0400

    Fixed accidental merge text

--- semantics/__test__/semantics.test.js
+++ semantics/__test__/semantics.test.js
@@ -4,18 +4,12 @@ const analyze = require('../analyzer');
 const program = String.raw`
 num x <- 2;
 x <- 5;
-<<<<<<< HEAD
 
 addFive(num a) -> num
   return 5;
 end
 
 num y <- addFive(5);`;
-=======
-_ apples <- "red";
-apples <- 4;
-`;
->>>>>>> 5fd07df1eedc9e4a8e99a8685b5ea64a741cd96d
 
 describe('The semantic analyzer', () => {
   test('accepts the mega program with all syntactic forms', done => {
