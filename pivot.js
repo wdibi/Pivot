2e913b986924eab07c29c6514359dc7e4074b588
Will DiBiagio <wdibiagio@gmail.com>
2020-04-29 03:01:18 -0500

    All gen javascript

--- pivot.js
+++ pivot.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
 const { argv } = require('yargs')
-  .usage('$0 [-a] [-o] [-i] filename')
-  .boolean(['a', 'o', 'i'])
+  .usage('$0 [-a] [-o] [-i] [-g] filename')
+  .boolean(['a', 'o', 'i', 'g'])
   .describe('a', 'show abstract syntax tree after parsing then stop')
   .describe('o', 'do optimizations')
   .describe('i', 'generate and show the intermediate code then stop')
-  // TOOD: Add generator command line
+  .describe('g', 'generate JavaScript code')
   .demand(1);
 
 const fs = require('fs');
@@ -14,7 +14,8 @@ const util = require('util');
 const { exec } = require('child_process');
 const parse = require('./ast/parser');
 const view = require('./semantics/viewer');
-const analyzer = require('./semantics/analyzer');
+const analyze = require('./semantics/analyzer');
+const generate = require('./generator/javascript-generator');
 // const optimizer = require('./semantics/optimizer');
 // require(`./backend/${argv.target}generator`);
 
@@ -41,15 +42,18 @@ fs.readFile(argv._[0], 'utf-8', (error, text) => {
   }
   if (argv.o) {
     try {
-      analyzer(program);
+      analyze(program);
       // program = optimizer(program);
     } catch (error) {
       console.error(error.message);
     }
   }
-  // if (argv.i) {
-  //   console.log(util.inspect(program, { depth: null }));
-  //   return;
-  // }
-  // program.gen();
+  if (argv.g) {
+    try {
+      analyze(program);
+      console.log(generate(program));
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
 });
